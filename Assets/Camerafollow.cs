4c6af0ac16d71d3bd2e03730905b2ddbda5f0859
Nguyễn Xuân Bảo <nxbao.9x@gmail.com>
2014-11-16 15:52:05 +0700

    Create

---
+++ Assets/Camerafollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camerafollow : MonoBehaviour {
+	private Transform player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Frog").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerpos = player.position;
+		playerpos.z = transform.position.z;
+		playerpos.y = transform.position.y;
+		transform.position = playerpos;
+	}
+}
---
+++ Assets/FrogJumping.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrogJumping : MonoBehaviour {
+	Vector3 velocity = Vector3.zero;
+	public int count;
+	public Vector3 jumpVel; 
+	public Vector3 gravity;
+	public float speed = 2500;
+	public float forwardSpeed = 50;
+	bool didJump = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+				forwardSpeed += Time.deltaTime*speed;
+		}
+		if(Input.GetKeyUp(KeyCode.Space))
+		{
+			didJump =true; 
+
+		}
+	}
+	void FixedUpdate ()
+	{
+		if (didJump) {
+			rigidbody2D.AddForce(Vector2.right*forwardSpeed);
+			rigidbody2D.AddForce(Vector2.up*forwardSpeed);
+			didJump = false;
+			forwardSpeed =50;
+				}
+		}
+}
